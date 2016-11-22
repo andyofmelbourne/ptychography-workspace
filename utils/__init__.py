1bf540e56172d043dd812fd6290d049de5889817
andyofmelbourne <andyofmelbourne@gmail.com>
2016-11-22 01:01:58 +0100

    big improvements to cpu stitch

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 import utils
-import pyximport; pyximport.install()
+import numpy
+import pyximport; pyximport.install(setup_args={"include_dirs":numpy.get_include()})
 
 from feature_matching import feature_map_cython
 from get_Fresnel_pixel_shifts_cxi import get_Fresnel_pixel_shifts_cxi
